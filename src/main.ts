b13b95a7ac9c6990594cce7e62d3c5049b36bc31
BryanGallo <bryangallo95@hotmail.com>
2024-12-14 14:41:47 -0500

    Usando GlobalPipes para validar los datos en el DTO
    * Se instalo class-validator y class-transformer

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  //?PIPE: se usa para trasformar datos y es para validacion
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, //? elimina los datos que no estan en el dto
+      forbidNonWhitelisted: true, //? envia un error si hay datos que no estan en el dto
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
