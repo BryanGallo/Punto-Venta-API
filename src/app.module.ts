cb6b49e221b52154a2d91f688935ea4e5fab808d
BryanGallo <bryangallo95@hotmail.com>
2024-12-15 23:59:17 -0500

    Implementando typeOrmConfig para usar el archivo de typeorm.config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CategoriesModule } from './categories/categories.module';
+import { typeOrmConfig } from './config/typeorm.config';
 
 @Module({
   controllers: [AppController],
@@ -11,6 +13,11 @@ import { CategoriesModule } from './categories/categories.module';
     ConfigModule.forRoot({
       isGlobal: true,
       // envFilePath: '.env',
+    }),
+    //? usando Fn TypeOrmModule para poderle pasar en los imports porque es un modulo, debe estar despues de las configuracion de variables de entorno pero antes de otros Modulos
+    TypeOrmModule.forRootAsync({
+      useFactory: typeOrmConfig,
+      inject: [ConfigService],
     }),
     CategoriesModule,
   ],
