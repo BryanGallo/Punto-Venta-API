a41353655e3f37da8f0129c7c1d287f21bbc91a0
BryanGallo <bryangallo95@hotmail.com>
2024-12-16 01:10:04 -0500

    actualizando a configService usado en inject de app.module

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,10 +1,14 @@
+import { ConfigService } from '@nestjs/config';
 import type { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
-export const typeOrmConfig = (): TypeOrmModuleOptions => ({
+export const typeOrmConfig = (
+  configService: ConfigService,
+): TypeOrmModuleOptions => ({
   type: 'postgres',
-  host: process.env.DB_HOST,
-  port: +process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
+  host: configService.get('DB_HOST'),
+  port: configService.get('DB_PORT'),
+  database: configService.get('DB_NAME'),
+  username: configService.get('DB_USERNAME'),
+  password: configService.get('DB_PASSWORD'),
+  // logging: true,
 });
