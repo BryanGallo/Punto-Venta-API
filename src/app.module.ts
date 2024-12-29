b03a13d5d15427ee538260ab13acfbfb96136175
BryanGallo <bryangallo95@hotmail.com>
2024-12-28 23:48:47 -0500

    CRUD de products, Entity configurada

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CategoriesModule } from './categories/categories.module';
 import { typeOrmConfig } from './config/typeorm.config';
+import { ProductModule } from './product/product.module';
 
 @Module({
   controllers: [AppController],
@@ -20,6 +21,7 @@ import { typeOrmConfig } from './config/typeorm.config';
       inject: [ConfigService],
     }),
     CategoriesModule,
+    ProductModule,
   ],
 })
 export class AppModule {}
