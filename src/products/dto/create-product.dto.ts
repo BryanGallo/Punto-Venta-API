807ca86f13b0b96a6c63ca91bf4c2d67b5b2f582
BryanGallo <bryangallo95@hotmail.com>
2025-01-01 20:19:02 -0500

    Agregando DTO al crear Producto

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,22 @@
-export class CreateProductDto {}
+import { IsInt, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { Category } from 'src/categories/entities/category.entity';
+
+export class CreateProductDto {
+  @IsString({ message: 'Nombre no valido' })
+  @IsNotEmpty({ message: 'El nombre es obligatorio' })
+  name: string;
+
+  @IsNumber({ maxDecimalPlaces: 2 }, { message: 'Precio no valido' })
+  @IsNotEmpty({ message: 'El precio es obligatorio' })
+  price: number;
+
+  @IsNumber({ maxDecimalPlaces: 0 }, { message: 'Cantidad no valido' })
+  @IsNotEmpty({ message: 'La cantidad es obligatoria' })
+  @IsNumber()
+  inventory: number;
+
+  //? colocamos el nombre de la columna que se creo en la tabla en este caso categoryId
+  @IsInt({ message: 'Categoría no valida' })
+  @IsNotEmpty({ message: 'La categorría es obligatoria' })
+  categoryId: Category;
+}
