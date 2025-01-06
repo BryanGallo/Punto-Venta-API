90ee00cebc4ec755e136dc8e9ce26e6264eb6a32
BryanGallo <bryangallo95@hotmail.com>
2025-01-05 20:28:51 -0500

    Generando create-user.dto y usando en controller y service
    * Ser elimino create y update.dto

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,15 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { CreateUserDto } from './dto/create-user.dto';
+
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('register')
-  create(@Body() createAuthDto: CreateAuthDto) {
-    return this.authService.create(createAuthDto);
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.authService.create(createUserDto);
   }
 
   @Get()
@@ -22,10 +22,10 @@ export class AuthController {
     return this.authService.findOne(+id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
-    return this.authService.update(+id, updateAuthDto);
-  }
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
+  //   return this.authService.update(+id, updateAuthDto);
+  // }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
---
+++ src/auth/dto/create-user.dto.ts
@@ -0,0 +1,29 @@
+import {
+    IsEmail,
+    IsNotEmpty,
+    IsString,
+    Matches,
+    MaxLength,
+    MinLength
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsString({ message: 'Nombre no valido' })
+  @IsNotEmpty({ message: 'El nombre es obligatorio' })
+  name: string;
+
+  @IsString({ message: 'Email no valido' })
+  @IsEmail({}, { message: 'No posee estructura de email' })
+  @IsNotEmpty({ message: 'El email es obligatorio' })
+  email: string;
+
+  @IsString({ message: 'Contraseña no válida' })
+  @IsNotEmpty({ message: 'La contraseña es obligatoria' })
+  @MinLength(6,{ message: 'La contraseña debe ser de mínimo de 6 caracteres'})
+  @MaxLength(50,{ message: 'La contraseña debe ser de máximo de 50 caracteres'})
+  @Matches(/(?:(?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message:
+      'La contraseña debe tener una letra Mayuscula, una letra minuscula y un número',
+  })
+  password: string;
+}
