0545c09ed93e40df668fb9054c03c2afb9b148e9
BryanGallo <bryangallo95@hotmail.com>
2025-01-05 20:27:51 -0500

    Definiendo Entidad User

--- src/auth/entities/user.entity.ts
+++ src/auth/entities/user.entity.ts
@@ -1,6 +1,22 @@
-import { Entity } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
+  @Column({ type: 'varchar', length: 120 })
+  name: string;
+
+  @Column({ type: 'varchar', unique: true })
+  email: string;
+
+  @Column({ type: 'text' })
+  password: string;
+
+  @Column({ type: 'boolean', default: true })
+  isActive: boolean;
+
+  @Column({ type: 'text', array: true })
+  roles: string[];
 }
