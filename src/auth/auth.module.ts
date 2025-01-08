e793dd8c7d64a82d75c0b411c9a04ff4e9fa9141
BryanGallo <bryangallo95@hotmail.com>
2025-01-08 15:31:33 -0500

    Usando el metodo registerAsync de JWT

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,21 +1,44 @@
 import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './entities/user.entity';
-import { PassportModule } from '@nestjs/passport';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { User } from './entities/user.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User]),
     PassportModule.register({ defaultStrategy: 'jwt' }),
-    // JwtModule.register({
-    //   secret: process.env.JWT_SECRET,
-    //   signOptions: {
-    //     expiresIn: '2h',
-    //   },
-    // }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        //*Del configService requiere usar el import y el inject
+        // console.log("Del config",configService.get('JWT_SECRET'));
+        //*Directamente de las variables de entorno
+        // console.log("V entorno",process.env.JWT_SECRET);
+
+        return {
+          //?Usarmos la primera opcion ene se caso pero cualquierda de las 2 funciona
+          secret: configService.get('JWT_SECRET'),
+          // secret: process.env.JWT_SECRET,
+          signOptions: {
+            expiresIn: '2h',
+          },
+        };
+      },
+
+      // useFactory: () => {
+      //   return {
+      //     secret: process.env.JWT_SECRET,
+      //     signOptions: {
+      //       expiresIn: '2h',
+      //     },
+      //   };
+      // },
+    }),
   ],
   controllers: [AuthController],
   providers: [AuthService],
