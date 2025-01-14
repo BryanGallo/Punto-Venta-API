73c410ed71982fc9249d6f83d9787e24b95691f9
BryanGallo <bryangallo95@hotmail.com>
2025-01-14 12:10:45 -0500

    Metodo generico para validar Roles
    * Leyendo metadata para obtener los roles almacenados

--- src/auth/guards/user-role/user-role.guard.ts
+++ src/auth/guards/user-role/user-role.guard.ts
@@ -1,11 +1,22 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { Observable } from 'rxjs';
 
 @Injectable()
 export class UserRoleGuard implements CanActivate {
+  constructor(
+    //? Nos permite obtener informacion de metadata de donde se coloque
+    private readonly reflector: Reflector,
+  ) {}
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
+    const validRoles: string[] = this.reflector.get(
+      'roles',
+      context.getHandler(),
+    );
+
+
     return true;
   }
 }
