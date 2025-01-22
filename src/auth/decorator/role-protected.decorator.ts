79d6292496e7386f4fbb1b996c726252c08b030f
BryanGallo <bryangallo95@hotmail.com>
2025-01-21 23:44:52 -0500

    Add: generamos enums para nuestros roles

--- src/auth/decorator/role-protected.decorator.ts
+++ src/auth/decorator/role-protected.decorator.ts
@@ -1,7 +1,8 @@
 import { SetMetadata } from '@nestjs/common';
 
+//? Asignamos a una variable para futuro si es necesario cambiar el nombre y solo tener que hacerlo aqui
 export const META_ROLES = 'roles';
 
 export const RoleProtected = (...args: string[]) => {
-  return SetMetadata('roles', args);
+  return SetMetadata(META_ROLES, args);
 };
