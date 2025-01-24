61eb24aef0baaf4f4b5f681f13048d0a9589f910
BryanGallo <bryangallo95@hotmail.com>
2025-01-23 21:53:30 -0500

    fix: exportando e importando categories en Products
    * Se realiza buenas practicas de importacion
    * Anteriormente se podia importar por la Entity que se colocaba el TypeOrmModule

--- src/categories/categories.module.ts
+++ src/categories/categories.module.ts
@@ -6,8 +6,9 @@ import { Category } from './entities/category.entity';
 import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports:[TypeOrmModule.forFeature([Category]),AuthModule],
+  imports: [TypeOrmModule.forFeature([Category]), AuthModule],
   controllers: [CategoriesController],
   providers: [CategoriesService],
+  exports: [TypeOrmModule],
 })
 export class CategoriesModule {}
