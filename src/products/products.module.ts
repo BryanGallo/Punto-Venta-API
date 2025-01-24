2d0c595e8d58cabbcc6a05fbdacdb6273c5aebb0
BryanGallo <bryangallo95@hotmail.com>
2025-01-23 21:54:40 -0500

    Product Parte del commit anterior

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Category } from 'src/categories/entities/category.entity';
+import { CategoriesModule } from 'src/categories/categories.module';
 import { Product } from './entities/product.entity';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
 
 @Module({
-  imports:[TypeOrmModule.forFeature([Category,Product])],
+  imports: [TypeOrmModule.forFeature([Product]), CategoriesModule],
   controllers: [ProductsController],
   providers: [ProductsService],
 })
