992588a6073b02d5a159ec2c98f8cef96d1c15b5
BryanGallo <bryangallo95@hotmail.com>
2025-02-03 22:14:44 -0500

    add: data para crear categorias y productos

---
+++ src/seeder/data/products.ts
@@ -0,0 +1,44 @@
+export const products = [
+  {
+    name: 'Sudadera Gris',
+    image: '1.jpg',
+    price: 49,
+    inventory: 5,
+    categoryId: 1,
+  },
+  {
+    name: 'Sudadera Naranja Hombre',
+    image: '2.jpg',
+    price: 29,
+    inventory: 3,
+    categoryId: 1,
+  },
+  {
+    name: 'Sudadera Azul',
+    image: '3.jpg',
+    price: 49,
+    inventory: 8,
+    categoryId: 1,
+  },
+  {
+    name: 'Tenis Mujer',
+    image: '10.jpg',
+    price: 59,
+    inventory: 10,
+    categoryId: 2,
+  },
+  {
+    name: 'Zapato Casual',
+    image: '11.jpg',
+    price: 49,
+    inventory: 10,
+    categoryId: 2,
+  },
+  {
+    name: 'Tenis Mujer Rosa',
+    image: '12.jpg',
+    price: 49,
+    inventory: 10,
+    categoryId: 2,
+  },
+];
