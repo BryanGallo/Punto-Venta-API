e2d7f2847ed4d4dda2b038689c0cba22e31750e6
BryanGallo <bryangallo95@hotmail.com>
2025-02-04 23:00:07 -0500

    add: seed creando categorias

--- src/seeder/seeder.service.ts
+++ src/seeder/seeder.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Category } from 'src/categories/entities/category.entity';
+import { Product } from 'src/products/entities/product.entity';
+import { Repository } from 'typeorm';
+import { categories } from './data/categories';
 
 @Injectable()
 export class SeederService {
+  constructor(
+    @InjectRepository(Category)
+    private readonly categoryReporitory: Repository<Category>,
+    @InjectRepository(Product)
+    private readonly productReporitory: Repository<Product>,
+  ) {}
   async seed() {
     console.log('desde seed');
+
+    await this.categoryReporitory.create(categories);
   }
 }
