9c7bcec1f22f4f217be8cf160f3e0e6556499d9d
BryanGallo <bryangallo95@hotmail.com>
2025-02-05 23:27:18 -0500

    add: seed creando roles

--- src/seeder/seeder.service.ts
+++ src/seeder/seeder.service.ts
@@ -4,10 +4,14 @@ import { Category } from 'src/categories/entities/category.entity';
 import { Product } from 'src/products/entities/product.entity';
 import { Repository } from 'typeorm';
 import { categories } from './data/categories';
+import { Role } from 'src/roles/entities/role.entity';
+import { roles } from './data/roles';
 
 @Injectable()
 export class SeederService {
   constructor(
+    @InjectRepository(Role)
+    private readonly roleReporitory: Repository<Role>,
     @InjectRepository(Category)
     private readonly categoryReporitory: Repository<Category>,
     @InjectRepository(Product)
@@ -15,7 +19,7 @@ export class SeederService {
   ) {}
   async seed() {
     console.log('desde seed');
-
+    await this.roleReporitory.create(roles);
     await this.categoryReporitory.create(categories);
   }
 }
