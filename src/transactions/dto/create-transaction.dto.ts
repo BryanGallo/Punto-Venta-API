a89a75228141ea6f54e63124dde5a15d978ebfd4
BryanGallo <bryangallo95@hotmail.com>
2025-02-14 00:18:15 -0500

    add: DTO para crear transaccion con sus contents
    * Manejo de datos complejos

--- src/transactions/dto/create-transaction.dto.ts
+++ src/transactions/dto/create-transaction.dto.ts
@@ -1,1 +1,38 @@
-export class CreateTransactionDto {}
+import { Type } from 'class-transformer';
+import {
+    ArrayNotEmpty,
+    IsArray,
+    IsInt,
+    IsNotEmpty,
+    IsNumber,
+    ValidateNested,
+} from 'class-validator';
+
+export class CreateTransactionDto {
+  @IsNotEmpty({ message: 'El Total no puede ir vacio' })
+  @IsNumber({ maxDecimalPlaces: 2 }, { message: 'Total no válido' })
+  total: number;
+
+  @IsArray({ message: 'Contenido no valido' })
+  @ArrayNotEmpty({ message: 'Los Contenidos no pueden ir vacios' })
+  @ValidateNested() // Valida cada elemento del array
+  @Type(() => TransactionContentsDto) // Especifica el tipo de los elementos
+  transactionContents: TransactionContentsDto[];
+}
+
+export class TransactionContentsDto {
+  @IsNotEmpty({ message: 'El ID del producto no puede estar vacío' })
+  @IsInt({ message: 'Id no válido' })
+  id: number;
+
+  @IsNotEmpty({ message: 'Cantidad no puede estar vacía' })
+  @IsNumber({}, { message: 'Cantidad no válida' })
+  quantity: number;
+
+  @IsNotEmpty({ message: 'Precio no puede estar vacío' })
+  @IsNumber({ maxDecimalPlaces: 2 }, { message: 'Precio no válido' })
+  price: number;
+
+  @IsNumber({}, { message: 'Id no valido' })
+  productId: number;
+}
--- src/transactions/transactions.module.ts
+++ src/transactions/transactions.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TransactionsService } from './transactions.service';
 import { TransactionsController } from './transactions.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Transaction } from 'typeorm';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
+  // imports: [TypeOrmModule.forFeature([Transaction])],
+  imports: [AuthModule],
   controllers: [TransactionsController],
   providers: [TransactionsService],
 })
