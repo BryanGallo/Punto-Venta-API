6ca5da076d3e59d2d5501ecd8c2ce2f42f489067
BryanGallo <bryangallo95@hotmail.com>
2025-03-13 23:39:00 -0500

    fix: Eliminando entity y dtos innecesarios  de reports
    * Respectivas importaciones

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,5 +1,4 @@
 import { Body, Controller, Post, Res } from '@nestjs/common';
-import { CreateReportDto } from './dto/create-report.dto';
 import { ReportsService } from './reports.service';
 import { Response } from 'express';
 
@@ -8,16 +7,11 @@ export class ReportsController {
   constructor(private readonly reportsService: ReportsService) {}
 
   @Post()
-  async create(
-    @Body() createReportDto: CreateReportDto,
-    @Res() response: Response,
-  ) {
+  async create(@Res() response: Response) {
     const docPdf = await this.reportsService.create();
 
-    response.setHeader('Content-Type', 'application/pdf')
-    docPdf.pipe(response)
-    docPdf.end()
-
-    
+    response.setHeader('Content-Type', 'application/pdf');
+    docPdf.pipe(response);
+    docPdf.end();
   }
 }
--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,15 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import type { TDocumentDefinitions } from 'pdfmake/interfaces';
 import { PrinterService } from 'src/printer/printer.service';
-import { CreateReportDto } from './dto/create-report.dto';
 
 @Injectable()
 export class ReportsService {
   constructor(private readonly printerService: PrinterService) {}
   async create() {
-    const docDefinition : TDocumentDefinitions = {
-      content:['Hola desde MakePdf']
-    }
+    const docDefinition: TDocumentDefinitions = {
+      content: ['Hola desde MakePdf'],
+    };
 
     const doc = this.printerService.createPdf(docDefinition);
 
