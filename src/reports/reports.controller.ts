03833c21d32429d1396d421f48db0513ce96dc08
BryanGallo <bryangallo95@hotmail.com>
2025-03-18 22:16:37 -0500

    add: Agregando autenticacion al reporte

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,12 +1,14 @@
 import { Body, Controller, Post, Res, ParseIntPipe } from '@nestjs/common';
 import { ReportsService } from './reports.service';
 import { Response } from 'express';
+import { Auth } from '../auth/decorator/auth.decorator';
 
 @Controller('reports')
 export class ReportsController {
   constructor(private readonly reportsService: ReportsService) {}
 
   @Post()
+  @Auth()
   async testPdf(@Body('name') name: string, @Res() response: Response) {
     const docPdf = await this.reportsService.testPdf(name);
 
--- src/reports/reports.module.ts
+++ src/reports/reports.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ReportsService } from './reports.service';
 import { ReportsController } from './reports.controller';
 import { PrinterModule } from 'src/printer/printer.module';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports:[PrinterModule],
+  imports: [PrinterModule, AuthModule],
   controllers: [ReportsController],
   providers: [ReportsService],
 })
